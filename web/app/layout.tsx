e913abbfdb0a84ee38d778443f986a5326fcec13
shafu <shafu0x@gmail.com>
2026-08-22 16:39:40 -0400

    Notify Discord when landing page links are clicked.

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ClickNotifier } from "@/components/click-notifier";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -31,7 +32,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <ClickNotifier />
+      </body>
     </html>
   );
 }
